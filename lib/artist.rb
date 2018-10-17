a8b78ef59570f15c9db2664897318f5b5217b01b
Austin  Burke <aburk3@gmail.com>
2018-10-17 21:59:10 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,19 +5,14 @@ class Artist
 
   def initialize(name)
     @name = name
-    @@all << self
   end
 
   def self.all
     @@all
   end
 
-  def new_song(name, genre)
-    Song.new(name, self, genre)
-  end
+  def new_song(name)
 
-  def songs
-    
   end
 
 end
