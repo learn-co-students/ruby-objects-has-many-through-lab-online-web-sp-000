cd82ee06e8dafedfe78c056ff5b6d1b15fdfd3f6
Rafael Batista <rafaelb24@yahoo.com>
2018-11-04 21:53:53 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment
+  attr_accessor :date, :patient, :doctor
+  @@all = []
+  def self.all
+    @@all
+  end
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    self.class.all << self
+  end
+end
