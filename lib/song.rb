cc7823e95db372a673dffd2fb39d5a2444ec9d96
Meisuleen Kased <meisuleen@gmail.com>
2018-12-21 17:56:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
     @@all = []
 
-  def initialize(name, genre, artist)
+  def initialize(name, artist, genre)
     @name = name
     @genre = genre
     @artist = artist
