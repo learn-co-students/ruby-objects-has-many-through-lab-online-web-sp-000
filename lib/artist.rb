c416a405dcd65c2afc02f71ca72862ba80a5475d
Ariel <arielord@buffalo.edu>
2018-12-24 21:51:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,16 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
   
   def initialize(name)
     self.name = name
+    self.songs = []
     @@all << self
   end
   
-  def new_song(song_title)
-    song = Song.new(song_title)
+  def new_song(song_title, genre)
+    song = Song.new(song_title, self, genre)
+    self.songs << song
   end
   
   def self.all
