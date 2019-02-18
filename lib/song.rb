7e58c0fb919cfb849aa55ab13b0d2790228773ea
Alexander Baker <alex.baker685@gmail.com>
2019-02-18 23:19:25 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :genre, :artist
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @artist = artist
+    @genre = genre
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
