fa5743ad84210b11d43debf0cb73d2e2d033b392
Chovin Carlson <Chovin@users.noreply.github.com>
2019-02-19 23:40:24 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+  
+  def doctors
+    self.appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+end
