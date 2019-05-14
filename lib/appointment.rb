e133a6369c800c90457fbb8977a00a39f7664996
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-14 22:59:52 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment
+  @@all = []
+  attr_reader :patient, :doctor
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |app|
+      app
+    end
+  end
+end 
