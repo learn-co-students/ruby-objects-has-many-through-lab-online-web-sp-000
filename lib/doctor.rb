643a45fdaaf3beda89144e245e3570cf1b892f58
Andrew Silva <andrewsilva9@gmail.com>
2019-05-19 17:50:14 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def patients
+    self.patient
+  end
+end
