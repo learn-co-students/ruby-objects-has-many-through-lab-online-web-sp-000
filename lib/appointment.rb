a233bf1a64e77af2db55860cb1dceb7ab600a762
Anthony Tilelli <anthony@tilelli.me>
2019-10-24 01:20:15 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,2 @@
+class appointment
+end
---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @all
+  end
+  
+  def new_song(song_name, song_genre)
+  end
+  
+  def songs
+  end
+
+  def genre
+  end
+  
+end
