2494fd806c8bd100a94121800debf90780db6d93
github username <github email address>
2019-11-05 13:00:24 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+def artist
+
+    @@all = []
+
+    def initialize()
+        @@all << self
+    end
+
+    def self.all
+        @all
+    end
+
+    def new_song
+
+    end
+/
+end
---
+++ lib/patient.rb
@@ -0,0 +1,2 @@
+def patient
+end
