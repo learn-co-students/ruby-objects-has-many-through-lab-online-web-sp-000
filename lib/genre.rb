9c8a84b7cd3f909f6593a4225bcc88f823875826
Becky Zorn <bczorn@gmail.com>
2020-01-30 20:41:01 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,30 @@
+class Genre
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+  
+  def artists
+    l = []
+    songs.each do |x|
+      l << x.artist
+    end
+    l
+  end
+  
+end
