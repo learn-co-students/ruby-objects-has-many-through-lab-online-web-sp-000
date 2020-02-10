8eddd915795084ef5b80d623eb11d5da6027ec06
Amy Fong <amyhfong@gmail.com>
2020-02-10 18:27:47 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+  attr_accessor :name, :song
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,11 @@
+class Genre
+
+  @@all = []
+
+  
+
+  def self.all
+    @@all
+  end
+
+end
