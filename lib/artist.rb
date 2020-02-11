d222aaf0fec5178595acee9eba0f1bd1b8fbb319
Amy Fong <amyhfong@gmail.com>
2020-02-11 16:45:12 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,6 +16,12 @@ class Artist
     Song.new(name, self, genre)
   end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
+  def genres
+    songs.collect {|song| song.genre}
+  end
 
 end
