90ba768b57096b722cb60e41195d626711667c5d
Alvin Coulson <aaacoulson@gmail.com>
2020-03-03 19:43:20 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Genre
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre.name}
+  end
+  
+  def artists
+    songs.map {|song| song.artist}
+  end
+end
