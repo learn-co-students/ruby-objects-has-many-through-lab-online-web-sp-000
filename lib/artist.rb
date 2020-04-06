ac91e18e461bf74294d72bfb7bdf1955760e0add
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-06 15:52:38 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def new_song(name, genre)
+    newsong = Song.new(name, self, genre)
+    newsong
+  end
+
+  def genres
+    songs.collect do |song|
+      song.genre
+    end
+  end
+
+end
