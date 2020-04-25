25a31fcca73fad195915aeafb03d2c9c067e6e9e
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-25 15:07:07 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres
+    Song.all.select {|genre| song.genre == self}
+  end
+  
+end
