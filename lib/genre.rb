d2c88dacee47d71ebc08fd73b8052878beb06eb8
Jake G <jake098877@gmail.com>
2020-05-18 00:36:14 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre
+  attr_accessor :name, :genre, :artists
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre = self
+
+
+    end
+  end
+
+    def artists
+      songs.collect do |song|
+      song.artist
+    end
+      # binding.pry
+
+    end
+end
