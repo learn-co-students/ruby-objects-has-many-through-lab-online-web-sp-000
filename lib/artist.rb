88a92a9e2867c5468bf31958df882b672055a7ec
asb1512 <andrew@olympicmedia.com>
2020-05-20 12:51:31 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        # binding.pry
+    end
+
+    def genres
+        songs.map do |song|
+            song.genre
+        end
+    end
+
+end
