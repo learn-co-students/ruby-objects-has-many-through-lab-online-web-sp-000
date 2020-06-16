87938f92e027402ace45ad8c419a2a26a547435d
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-16 15:30:26 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    doctor = self
+    Appointment.new(date, patient, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor = self}
+  end
+
+  def patients
+    self.appointments.collect {|appointment| appointment.patient}
+  end
+
+end
