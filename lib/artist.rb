c09e5df4b057345b16587205cc2b27abd6305b99
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-07-29 00:33:29 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    genres = []
+    self.songs.each do |song|
+      genres << song.genre
+    end
+    genres
+  end
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    patients = []
+    self.appointments.each do |appointment|
+      patients << appointment.patient
+    end
+    patients
+  end
+end
