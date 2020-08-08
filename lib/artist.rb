6160fa7f7460ab9a97ba662a38c54c2747e8e2c3
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-08 00:06:34 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  def songs 
+    Song.all.select do |s|
+      s.artist == self 
+    end 
+  end
+  
+  def genres 
+    types = songs.collect do |s|
+      s.genre 
+    end 
+    types
+  end
+  
+end 
