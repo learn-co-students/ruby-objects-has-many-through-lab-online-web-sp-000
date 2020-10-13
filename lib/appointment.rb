869dbcc44af3da6f754b44ef818ff9af572f97a6
Alan Tang <giabao_tang@yahoo.com>
2020-10-13 23:05:02 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment 
+  attr_accessor :date, :doctor, :patient 
+  @@all = []
+  def initialize(date, patient, doctor)
+    @time = date 
+    @patient = patient 
+    @doctor = doctor
+    @@all << self
+  end
+  def self.all 
+    @@all 
+  end
+end
