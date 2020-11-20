daf54538bdd4c2c471b42399c16248bab539c395
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-11-20 00:40:29 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def name
+    @name
+  end
+
+  def songs
+
+  end
+end
