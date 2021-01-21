44bca11bd547e3beaa54330bbf5cacdc6ff7a8b0
Clint Richards <richardsclint17@gmail.com>
2021-01-21 02:29:32 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,23 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+    self.appointments.map { |appoint| appoint.patient }.uniq
+  end
+end
