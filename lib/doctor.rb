e5d9ab9acd02c73fb87508b53726ee2dbd1bc4b3
danpaslawsky <danpaslawsky@gmail.com>
2021-05-20 10:26:07 -0700

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,38 @@
+require 'pry'
+class Doctor
+
+    attr_accessor :name, :appointment, :patient
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+    
+    
+    def new_appointment(date, patient)
+        #binding.pry
+        Appointment.new(patient, self, date)
+      
+    end
+  
+    def appointments
+      Appointment.all.select do |app|
+        app.doctor == self
+
+      end
+      #binding.pry
+    end
+  
+    def patients
+      appointments.map do |appointment|
+        appointment.patients
+      end
+    end
+  
+  end
